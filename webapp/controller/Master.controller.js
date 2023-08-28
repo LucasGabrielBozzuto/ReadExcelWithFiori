c71877325406249bcb9b5e192872218df4132112
LucasGabrielBozzuto <132913618+LucasGabrielBozzuto@users.noreply.github.com>
2023-08-28 12:15:25 -0300

    Codigo funcionando
    Se cargan bien los archivos

--- webapp/controller/Master.controller.js
+++ webapp/controller/Master.controller.js
@@ -2,34 +2,35 @@ sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/ui/model/json/JSONModel",
 	"sap/ui/model/Filter",
-	"sap/ui/model/FilterOperator"
-], function (Controller, JSONModel, Filter, FilterOperator) {
+	"sap/ui/model/FilterOperator",
+	"sap/m/MessageToast"
+], function (Controller, JSONModel, Filter, FilterOperator,MessageToast) {
 	"use strict";
 
 	return Controller.extend("LGB4.Carpinteria.controller.Master", {
 		onInit: function () {
-			let oModel = new JSONModel([]);
+			var oModel = new JSONModel([]);
 			this.getView().setModel(oModel, "excelModel");
 		},
 		uploadExcel: function(oEvent){
 			this._importExcel(oEvent.getParameter("files"), 0)
 		},
 		_importExcel: function(files, index){
-			let file = files[index];
+			var file = files[index];
 			
 			if(file && window.FileReader){
-				let excelData = [];
-				let reader = new FileReader();
+				var excelData = [];
+				var reader = new FileReader();
 				
 				reader.onload = function (oEvent){
-					let data = oEvent.target.result;
-					let workbook = XLSX.read(data, {
+					var data = oEvent.target.result;
+					var workbook = XLSX.read(data, {
 						type: 'binary'	
 					});
-					let excelModel = this.getView().getModel("excelModel");
-					let json = excelModel.getData();
+					var excelModel = this.getView().getModel("excelModel");
+					var json = excelModel.getData();
 					
-					let lastId = 1;
+					var lastId = 1;
 					
 					if (json.length > 0){
 						lastId = json[json.length - 1].id + 1;
@@ -42,7 +43,8 @@ sap.ui.define([
 							excelData[i].id = lastId;
 							lastId = lastId + 1;
 							excelData[i].IDLOCAL = excelData[i]["IDLOCAL"];
-							excelData[i].NOMBRELOCAL = excelData[i]["NOMBRELOCAL"];
+							excelData[i].NOMBREPRODUCTO = excelData[i]["NOMBREPRODUCTO"];
+							excelData[i].VALOR = excelData[i]["VALOR"];
 							json.push(excelData[i]);
 						}
 						excelModel.refresh();
@@ -63,17 +65,33 @@ sap.ui.define([
 			var OData = this.getView().getModel();
 			var that = this;
 			var Exceldata = this.getView().getModel("excelModel").getData();
-			var data = {
-				IDLOCAL : "",
-				NOMBRELOCAL : ""
-			};
-			var sPath = "/LOCALSet";
+			var sPath = "/PRODUCTOSet";
+			OData.setUseBatch(false);
+			/*var data = {
+				"Nombrelocal" : "ejemplo"
+			}
+			
+			OData.create(sPath, data, {
+	            success: function (response) {
+	                debugger;
+				},
+				error: function(error){
+					debugger;
+				}
+				
+			});*/
+			
 			
 			
 			for (var i=0; i<Exceldata.length; i++){
-				data.IDLOCAL = Exceldata[i].IDLOCAL.toString();
-				data.NOMBRELOCAL = Exceldata[i].NOMBRELOCAL.toString();
-				
+				var data =	{
+					"Idlocal" : "",
+					"Nombreproducto" : "",
+					"Valor" : ""
+				};
+				data.Idlocal = Exceldata[i].IDLOCAL;
+				data.Nombreproducto = Exceldata[i].NOMBREPRODUCTO;
+				data.Valor = Exceldata[i].VALOR;
 				OData.create(sPath,data, {
                     success: function (response) {
                         debugger;
@@ -84,6 +102,7 @@ sap.ui.define([
 			
 				}
 			)};
+			
 		}
 	})
 })
